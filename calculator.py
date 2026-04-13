c4cae22696fa1bc97f46b605afafae6d37886c0a
poss637 <cbean9493@gmail.com>
2026-04-13 03:07:46 +0200

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,16 @@
-calcultor.py
+print("Simple Calculator")
+
+num1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num2 = float(input("Enter second number: "))
+
+if operator == "+":
+    print("Result:", num1 + num2)
+elif operator == "-":
+    print("Result:", num1 - num2)
+elif operator == "*":
+    print("Result:", num1 * num2)
+elif operator == "/":
+    print("Result:", num1 / num2)
+else:
+    print("Invalid operator")
