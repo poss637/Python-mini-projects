41212cfb916da9453268d29db9b07ea935b6ab8d
poss637 <cbean9493@gmail.com>
2026-04-13 02:55:52 +0200

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,1 +1,10 @@
-password_generator.py
+import random
+import string
+
+length = int(input("Enter password length: "))
+
+characters = string.ascii_letters + string.digits + string.punctuation
+
+password = "".join(random.choice(characters) for i in range(length))
+
+print("Generated password:", password)
